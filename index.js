05411e66b25d03f3d4e87056c308df433b352984
KobyS82 <kobysillito@gmail.com>
2023-01-13 18:02:32 -0600

    added questions

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 // TODO: Include packages needed for this application
-const express = require('express');
 const inquirer = require('inquirer');
 
-const app = express();
-
 // TODO: Create an array of questions for user input
-const questions = [
-    
-];
+const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is your Title',
+      name: 'title',
+    },
+    {
+      type: 'input',
+      message: `What is your description?
+      "This page will serve as..."`,
+      name: 'description',
+    },
+    {
+      type: 'input',
+      message: 'What are your installation instructions?',
+      name: 'installation',
+    },
+    {
+    type: 'input',
+    message: 'What are your usage instructions?',
+    name: 'usage',
+    },
+    {
+    type: 'input',
+    message: `Who are your credits?
+    ex:"Name, for helping with..., link to their page"`,
+    name: 'credits',
+    },
+  ])
+  .then((response) =>
+    response.credits === 'koby'
+      ? console.log('Success!')
+      : console.log('You forgot your password already?!')
+  );
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
