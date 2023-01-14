6804a2cb1d1e267370916eab301bbce9688587ba
KobyS82 <kobysillito@gmail.com>
2023-01-13 18:24:59 -0600

    added read me template

---
+++ READ-ME.md
@@ -0,0 +1,29 @@
+
+    # HELLOOO
+
+    ## Description
+
+    This page will serve as g
+
+    ## Table of contents
+
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[Cradits](#credits)
+
+
+    ## Installation
+
+    undefined
+
+
+    ## Usage
+
+    g
+
+
+    ## Credits
+
+    g
+
+    
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -34,13 +35,45 @@ inquirer
     },
   ])
   .then((response) =>
-    response.credits === 'koby'
-      ? console.log('Success!')
-      : console.log('You forgot your password already?!')
+    writeToFile(response)
   );
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data) {
+    const readMe = `
+    # ${data.title}
+
+    ## Description
+
+    This page will serve as ${data.description}
+
+    ## Table of contents
+
+    -[Installation](#installation)
+    -[Usage](#usage)
+    -[Cradits](#credits)
+
+
+    ## Installation
+
+    ${data.istallation}
+
+
+    ## Usage
+
+    ${data.usage}
+
+
+    ## Credits
+
+    ${data.credits}
+
+    `;
+    // Change location later
+    fs.writeFile('READ-ME.md', readMe, (err) =>
+    err ? console.error(err) : console.log('Success!')
+);
+}
 
 // TODO: Create a function to initialize app
 function init() {}
